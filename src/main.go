03324569a309531d30b222663ad93f8cc34f6500
Victor Nitu <nitu.v.r@gmail.com>
2020-03-20 20:58:20 +0100

    first commit

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"github.com/phiskills/grpc-api.go"
+	"github.com/phisuite/data.go"
+	"log"
+)
+
+type entityServer struct {
+	data.UnimplementedEntityAPIServer
+}
+
+func (e entityServer) Create(_ context.Context, entity *data.Entity) (*data.Entity, error) {
+	log.Printf("Create: %v", entity)
+	return entity, nil
+}
+
+func (e entityServer) Update(_ context.Context, entity *data.Entity) (*data.Entity, error) {
+	log.Printf("Update: %v", entity)
+	return entity, nil
+}
+
+func main() {
+	api := grpc.New()
+	data.RegisterEntityAPIServer(api.Server, &entityServer{})
+	api.Start()
+}
